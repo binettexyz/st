23f8a3cb8fdbcf82f119269f765568ad733cee48
binettexyz <46168797+binettexyz@users.noreply.github.com>
2022-09-06 08:00:24 -0400

    Test: trying to change icon size

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 static char *font = "mono:pixelsize=13:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-    "materialdesignicons-webfont:size=12"
+    "materialdesignicons-webfont:size=20"
 };
 
 static int borderpx = 2;
