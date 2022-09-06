b1466d1af3e110d5f197ea0f25316460b63e6bc5
binettexyz <46168797+binettexyz@users.noreply.github.com>
2022-09-05 22:29:44 -0400

    test: add fallback font

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 static char *font = "mono:pixelsize=13:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-    /* TODO: add material design font */
+    "materialdesignicons-webfont:size=12";
 };
 
 static int borderpx = 2;
